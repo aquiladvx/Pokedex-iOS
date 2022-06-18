f3f86b01c686710454f2aee9a2661f5676c2012d
Davi Aquila <daviaquila162@gmail.com>
2022-06-18 10:46:52 -0400

    Initial Commit

---
+++ pokedex-ios/pokedex_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedex_iosApp.swift
+//  pokedex-ios
+//
+//  Created by Davi Aquila on 2022-06-18.
+//
+
+import SwiftUI
+
+@main
+struct pokedex_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
